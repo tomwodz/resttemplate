002ba8542b71609e9cf01558868cb034f5c1ce41
twodzinski@op.pl <twodzinski@op.pl>
2023-06-22 18:50:41 +0200

    Resttemplate

--- src/main/java/pl/jjr/tomwodz/resttemplate/Config.java
+++ src/main/java/pl/jjr/tomwodz/resttemplate/Config.java
@@ -1,2 +1,14 @@
-package pl.jjr.tomwodz.resttemplate;public class Config {
+package pl.jjr.tomwodz.resttemplate;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class Config {
+
+    @Bean
+    RestTemplate restTempalte(){
+        return new RestTemplate();
+    }
 }
--- src/main/java/pl/jjr/tomwodz/resttemplate/ShawnMendesProxy.java
+++ src/main/java/pl/jjr/tomwodz/resttemplate/ShawnMendesProxy.java
@@ -1,2 +1,27 @@
-package pl.jjr.tomwodz.resttemplate;public class ShawnMendesProxy {
+package pl.jjr.tomwodz.resttemplate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class ShawnMendesProxy {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String makeShawnMendesRequest(String term, Integer limit) {
+        String uri = "https://itunes.apple.com/search?term=" + term+ "&limit=" + limit;
+        ResponseEntity<String> exchange = restTemplate.exchange(
+                uri,
+                HttpMethod.GET,
+                null,
+                String.class
+        );
+        return exchange.getBody();
+    }
+
+
 }
