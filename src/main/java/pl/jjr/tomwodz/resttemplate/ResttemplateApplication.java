fd44f8c7566e388b0c7a889a6dc9e76f9603238e
twodzinski@op.pl <twodzinski@op.pl>
2023-06-22 22:48:09 +0200

    try catch

--- src/main/java/pl/jjr/tomwodz/resttemplate/ResttemplateApplication.java
+++ src/main/java/pl/jjr/tomwodz/resttemplate/ResttemplateApplication.java
@@ -1,5 +1,7 @@
 package pl.jjr.tomwodz.resttemplate;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,9 +19,19 @@ public class ResttemplateApplication {
     }
 
     @EventListener(ApplicationStartedEvent.class)
-    public void makeRequestToShawnMendesEndpoint(){
-        String response = shawnMendenClient.makeShawnMendesRequest("shawnmendes", 3);
-        System.out.println(response);
+    public void makeRequestToShawnMendesEndpoint() throws JsonProcessingException {
+        String json = shawnMendenClient.makeShawnMendesRequest("shawnmendes", 4);
+        if(json != null){
+            ShawnMendesResponse shawnMendesResponse = mapJsonToShawnMendesResponse(json);
+            System.out.println(shawnMendesResponse);
+        }
+
     }
+
+    private ShawnMendesResponse mapJsonToShawnMendesResponse(String json) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, ShawnMendesResponse.class);
+    }
+
 
 }
--- src/main/java/pl/jjr/tomwodz/resttemplate/ShawnMendesProxy.java
+++ src/main/java/pl/jjr/tomwodz/resttemplate/ShawnMendesProxy.java
@@ -1,10 +1,14 @@
 package pl.jjr.tomwodz.resttemplate;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 @Component
@@ -16,15 +20,21 @@ public class ShawnMendesProxy {
     @Value("${addresswww.service.url}")
     String url;
 
-    public String makeShawnMendesRequest(String term, Integer limit) {
-        String uri = url + "/search?term=" + term+ "&limit=" + limit;
-        ResponseEntity<String> exchange = restTemplate.exchange(
-                uri,
-                HttpMethod.GET,
-                null,
-                String.class
-        );
-        return exchange.getBody();
+    public String makeShawnMendesRequest(String term, Integer limit) throws JsonProcessingException {
+        String uri = url + "/search?term=" + term + "&limit=" + limit;
+        return makeRequest(uri);
+    }
+
+    private String makeRequest(String uri) {
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.GET, null, String.class);
+            return response.getBody();
+        } catch (RestClientResponseException exception) {
+            System.out.println(exception.getStatusText() + " " + exception.getStatusCode().value());
+        } catch (RestClientException exception) {
+            System.out.println(exception.getMessage());
+        }
+        return null;
     }
 
 
