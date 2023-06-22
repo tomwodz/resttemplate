d8b49c2f2eff6dd0feeae0fd7d7d538115839742
twodzinski@op.pl <twodzinski@op.pl>
2023-06-22 22:48:09 +0200

    UriComponentsBuilder

--- src/main/java/pl/jjr/tomwodz/resttemplate/ShawnMendesProxy.java
+++ src/main/java/pl/jjr/tomwodz/resttemplate/ShawnMendesProxy.java
@@ -1,11 +1,18 @@
 package pl.jjr.tomwodz.resttemplate;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @Component
 public class ShawnMendesProxy {
@@ -16,15 +23,32 @@ public class ShawnMendesProxy {
     @Value("${addresswww.service.url}")
     String url;
 
-    public String makeShawnMendesRequest(String term, Integer limit) {
-        String uri = url + "/search?term=" + term+ "&limit=" + limit;
-        ResponseEntity<String> exchange = restTemplate.exchange(
-                uri,
-                HttpMethod.GET,
-                null,
-                String.class
-        );
-        return exchange.getBody();
+    public String makeShawnMendesRequest(String term, Integer limit) throws JsonProcessingException {
+        String uri = url + "/search?term=" + term + "&limit=" + limit;
+        return makeRequest(uri);
+    }
+
+    private String makeRequest(String uri) {
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .newInstance()
+                .scheme("https")
+                .host("itunes.apple.com")
+                .path("/search")
+                .queryParam("term", "shawnMendes")
+                .queryParam("limit", 1);
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(
+                    builder.build().toUri(),
+                    HttpMethod.GET,
+                    null,
+                    String.class);
+            return response.getBody();
+        } catch (RestClientResponseException exception) {
+            System.out.println(exception.getStatusText() + " " + exception.getStatusCode().value());
+        } catch (RestClientException exception) {
+            System.out.println(exception.getMessage());
+        }
+        return null;
     }
 
 
---
+++ src/main/java/pl/jjr/tomwodz/resttemplate/ShawnMendesResponse.java
@@ -0,0 +1,7 @@
+package pl.jjr.tomwodz.resttemplate;
+
+import java.util.List;
+
+public record ShawnMendesResponse(Integer resultCount, List<ShawnMendesResult> results) {
+
+}
